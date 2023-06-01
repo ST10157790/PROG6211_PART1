b9c0cf5391bf87d7656af9804603bbf0553f5cfa
lab_services_student <lab_services_student@lab000000>
2023-06-01 15:45:44 +0000

    Add project files.

---
+++ PROG6211_PART1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6211_PART1
+{
+   public class Program
+    {
+        static Ingredient[] ingredients;
+        static Step[] steps;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Recipe App!");
+
+            while (true)
+            {
+                Console.WriteLine("\nPlease enter the recipe details:");
+                Console.Write("Number of ingredients: ");
+                int numIngredients = Convert.ToInt32(Console.ReadLine());
+
+                ingredients = new Ingredient[numIngredients];
+                for (int i = 0; i < numIngredients; i++)
+                {
+                    Console.WriteLine($"\nIngredient {i + 1}:");
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Quantity: ");
+                    double quantity = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Unit of Measurement: ");
+                    string unit = Console.ReadLine();
+
+                    ingredients[i] = new Ingredient
+                    {
+                        Name = name,
+                        Quantity = quantity,
+                        Unit = unit
+                    };
+                }
+
+                Console.Write("\nNumber of steps: ");
+                int numSteps = Convert.ToInt32(Console.ReadLine());
+
+                steps = new Step[numSteps];
+                for (int i = 0; i < numSteps; i++)
+                {
+                    Console.WriteLine($"\nStep {i + 1}:");
+                    Console.Write("Description: ");
+                    string description = Console.ReadLine();
+
+                    steps[i] = new Step
+                    {
+                        Description = description
+                    };
+                }
+
+                DisplayRecipe();
+
+                Console.WriteLine("\nOptions:");
+                Console.WriteLine("1. Scale recipe");
+                Console.WriteLine("2. Reset quantities");
+                Console.WriteLine("3. Clear data and enter a new recipe");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        ScaleRecipe();
+                        break;
+                    case 2:
+                        ResetQuantities();
+                        break;
+                    case 3:
+                        ClearData();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void DisplayRecipe()
+        {
+            Console.WriteLine("\nRecipe Details:");
+            Console.WriteLine("Ingredients:");
+            foreach (Ingredient ingredient in ingredients)
+            {
+                Console.WriteLine($"{ingredient.Name}: {ingredient.Quantity} {ingredient.Unit}");
+            }
+
+            Console.WriteLine("\nSteps:");
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {steps[i].Description}");
+            }
+        }
+
+        static void ScaleRecipe()
+        {
+            Console.Write("\nEnter scaling factor (0.5, 2, or 3): ");
+            double factor = Convert.ToDouble(Console.ReadLine());
+
+            foreach (Ingredient ingredient in ingredients)
+            {
+                ingredient.Quantity *= factor;
+            }
+
+            DisplayRecipe();
+        }
+
+        static void ResetQuantities()
+        {
+            foreach (Ingredient ingredient in ingredients)
+            {
+                ingredient.Quantity /= ingredient.Quantity;
+            }
+
+            DisplayRecipe();
+        }
+
+        static void ClearData()
+        {
+            ingredients = null;
+            steps = null;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Open Visual Studio.
+Click on "Open a project or solution" on the start screen or go to "File" > "Open" > "Project/Solution".
+Navigate to the folder where you have the RecipeApp code stored and select the project file 
+(usually with a .csproj extension) and click "Open".
+Once the project is loaded, 
+make sure the RecipeApp project is selected as 
+the startup project. You can right-click on the RecipeApp 
+project in the Solution Explorer and choose "Set as StartUp Project" if it's not already set.
+Press F5 or click on the "Start" button in the toolbar to build and run the application.
+A console window will open, and you can interact with the application by following
+ the prompts and entering the required information.
+
+Note: Ensure that you have the necessary version of the .NET Framework installed on your machine to run the application.
